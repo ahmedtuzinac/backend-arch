133bdfcbbfdcbe0c74bd2d1701a7be537e7e4485
Ahmed Tuzinac <ahmed.tuzinac@digitalcube.rs>
2026-06-05 09:56:30 +0200

    feat: add infrastructure configs for PostgreSQL, Redis, and Nginx

---
+++ infra/postgres/init.sql
@@ -0,0 +1,7 @@
+-- Create databases for each service
+CREATE DATABASE auth_db;
+CREATE DATABASE websocket_db;
+
+-- Grant privileges
+GRANT ALL PRIVILEGES ON DATABASE auth_db TO postgres;
+GRANT ALL PRIVILEGES ON DATABASE websocket_db TO postgres;
